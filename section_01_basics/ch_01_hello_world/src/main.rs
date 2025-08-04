848c673f9d92b52b85231c211e245cced4e8f064
BoomTHDev <minecraft8231@gmail.com>
2025-08-04 20:26:46 +0700

    no success day 2 wait for learn

--- section_01_basics/ch_01_hello_world/src/main.rs
+++ section_01_basics/ch_01_hello_world/src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
     println!("Hello, world!");
+
+    print!("Hello, ");
+    print!("world!\n");
+    print!("\taaa\n");
+
+    println!("-- Base Output --");
+
+    println!("Welcome {} {}", "BoomTH", "Dev");
+
+    println!(
+        "{0} is a friend of {1}. And {1} is also a friend of {0}.",
+        "Alice", "Bob"
+    );
+
+    println!("-- Named Arguments --");
+    println!("My name is {name}", name = "Boom");
+    println!(
+        "I am {old} year old. I live in {city}.",
+        old = 21,
+        city = "Bangkok"
+    );
+
+    println!("-- Formatting Numbers --");
+    println!("Pi is approximately {:.2}", 3.14159);
+    println!("Pi is approximately {value:.2}", value = 3.14159);
+    println!("Number with padding: {:05}", 32);
+    println!("Binary: {:b}", 32);
+    println!("Hexadecimal: {:x}", 32);
+    println!("Octal: {:o}", 32);
+
+    println!("-- Formatting Text --");
+    println!("Left aligned: '{:<10}'", "Hello");
+    println!("Right aligned: '{:>10}'", "Hello");
+    println!("Center aligned: '{:^11}'", "Hello");
+
+    println!("-- Escape Characters --");
+    println!("Quote: \"Hello World\"");
+    println!("Backslash: \\");
+    println!("New line:\nSecond line");
+    println!("Tab:\tTabbed Text");
 }
--- section_01_basics/ch_02_types_variables/src/main.rs
+++ section_01_basics/ch_02_types_variables/src/main.rs
@@ -1,3 +1,51 @@
 fn main() {
-    println!("Hello, Ch 2!");
+    println!("-------------------------");
+
+    // Statically Typed และการอนุมานชนิดข้อมูล (Type Inference)
+    let a: i32 = 100;
+    let b = -200;
+
+    println!("integer with explicit type: {}", a);
+    println!("integer with inferred type: {}", b);
+
+    println!("-------------------------");
+
+    // Scalar Types: Floating-Points
+    let temp: f64 = 3.14159;
+    let presure: f32 = 10.984648;
+
+    println!("floating-point number: {}", temp);
+    println!("floating-point number: {}", presure);
+
+    println!("-------------------------");
+
+    // Signed integer สามารถติดลบได้
+    let balance: i32 = -500;
+    println!("Signed interger: {}", balance);
+
+    // Unsigned integer เก็บได้แค่ค่าบวกและศูนย์
+    let age: u8 = 0;
+    println!("Unsigned integer: {}", age);
+
+    println!("-------------------------");
+
+    // Boolean type
+    let is_active: bool = true;
+    let is_logged_in: bool = false;
+
+    println!("Is active: {}", is_active);
+    println!("Is logged in: {}", is_logged_in);
+
+    println!("-------------------------");
+
+    // Character type
+    let letter: char = 'R';
+
+    println!("Letter: {}", letter);
+
+    println!("-------------------------");
+
+    // Character type string
+    let greeting: String = "Hello, world!".to_string();
+    println!("Greeting: {}", greeting);
 }
