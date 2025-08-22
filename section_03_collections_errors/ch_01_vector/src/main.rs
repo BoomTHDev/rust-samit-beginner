503588f8dac46e15a48ac27809179fdba8d5936c
BoomTHDev <minecraft8231@gmail.com>
2025-08-22 21:36:18 +0700

    wait to next ep4

--- section_03_collections_errors/ch_01_vector/src/main.rs
+++ section_03_collections_errors/ch_01_vector/src/main.rs
@@ -6,4 +6,41 @@ fn main() {
     numbers.push(3);
 
     println!("{:?}", numbers);
+
+    let fruits = vec!["Apple", "Banana", "Orange"];
+    println!("{:?}", fruits);
+    println!("{:?}", fruits[1]);
+
+    let fruits2: Vec<String> = vec![
+        "Apple".into(),
+        "Banana".into(),
+        "Orange".into(),
+        "300".into(),
+        500.to_string(),
+    ];
+    println!("{:?}", fruits2);
+
+    let value: i32 = fruits2[4].parse().unwrap();
+    println!("{}", value * 2);
+
+    match numbers.get(1) {
+        Some(value) => println!("{}", value * 2),
+        None => println!("Index out of bounds"),
+    }
+
+    for fruit in &fruits {
+        println!("found fruit {}", fruit);
+    }
+
+    for number in &mut numbers {
+        *number *= 2;
+    }
+    println!("result after multipying by 2: {:?}", numbers);
+
+    // Array
+    let arr: [i32; 5] = [10, 20, 30, 40, 50];
+    println!("{:?}", arr);
+
+    let arr2: [String; 2] = ["BoomTH".to_string(), "Meen".to_string()];
+    println!("{:#?}", arr2);
 }
